da9144b69369e37abf19f13b530da12bb4862bdb
Kevin <kevin_a_ferrari@hotmail.com>
2018-10-14 23:41:17 +0700

    commit 14-10-2018

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql');
-const port = 1995;
+const port = 2018;
 
 var app = express();
 var url = bodyParser.urlencoded({ extended: false });
@@ -20,6 +20,10 @@ const conn = mysql.createConnection({
     port: 3306
 });
 
+app.get('/', (req,res) => {
+    res.send('<h1>API WMM Active!!!</h1>');
+})
+
 app.get('/login', (req,res) => {
     const { email, password } = req.query;
     var data = {
@@ -30,7 +34,7 @@ app.get('/login', (req,res) => {
     conn.query(sql, data, (err, results) => {
         if(err) throw err;
         res.send(results);
-        console.log(results);
+        // console.log(results);
     })
 })
 
@@ -45,6 +49,114 @@ app.post('/register', (req,res) => {
     conn.query(sql, data, (err,results) => {
         if(err) throw err;
         res.send(data);
+        // console.log(data);
+    })
+});
+
+app.get('/listmenu', (req,res) => {
+    var sql1 = `select iddaftarmenu, menu, description, 
+                harga, k.nama as kategori
+                from daftarmenu d join kategori k
+                on d.idkategori = k.id;`;
+    var sql2 = `select * from kategori;`;
+    conn.query(sql1, (err1, results1) => {
+        if(err1) throw err1;
+        // console.log(results1);
+
+        conn.query(sql2, (err2, results2) => {
+            if(err2) throw err2;
+            res.send({ daftarmenu: results1, kategori: results2 });
+            // console.log(results2);
+        })
+    })
+});
+
+app.get('/searchmenu', (req,res) => {
+    const { namakategori } = req.query;
+    var sql = `select iddaftarmenu, menu, description, 
+                harga, k.nama as kategori
+                from daftarmenu d join kategori k
+                on d.idkategori = k.id
+                where k.nama Like '%${namakategori}%';`;
+    conn.query(sql, (err, results) => {
+        if(err) throw err;
+
+        res.send(results);
+        // console.log(results);
+    })
+})
+
+app.get('/sortinghargaAsc', (req,res) => {
+    const { namakategori } = req.query;
+    if(namakategori !== ""){
+        var sql = `select iddaftarmenu, menu, description,
+                    harga, k.nama as kategori
+                    from daftarmenu d join kategori k
+                    on d.idkategori = k.id
+                    WHERE k.nama LIKE '%${namakategori}%'
+                    order by harga;`;
+
+        conn.query(sql, (err, results) => {
+            if(err) throw err;
+
+            res.send(results);
+            // console.log(results);
+        })
+    }
+    else{
+        sql = `select iddaftarmenu, menu, description, 
+                harga, k.nama as kategori
+                from daftarmenu d join kategori k
+                on d.idkategori = k.id
+                order by harga;`;
+        conn.query(sql, (err, results) => {
+            if(err) throw err;
+
+            res.send(results);
+            // console.log(results1);
+        })
+    }
+})
+
+app.get('/sortinghargaDesc', (req,res) => {
+    var sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori
+                from daftarmenu d
+                join kategori k
+                on d.idkategori = k.id
+                order by harga DESC;`;
+    conn.query(sql, (err, results) => {
+        if(err) throw err;
+
+        res.send(results);
+        console.log(results);
+    })
+})
+
+app.get('/sortingKtgAsc', (req,res) => {
+    var sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori
+                from daftarmenu d
+                join kategori k
+                on d.idkategori = k.id
+                order by kategori;`;
+    conn.query(sql, (err, results) => {
+        if(err) throw err;
+
+        res.send(results);
+        console.log(results);
+    })
+})
+
+app.get('/sortingKtgDesc', (req,res) => {
+    var sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori
+                from daftarmenu d
+                join kategori k
+                on d.idkategori = k.id
+                order by kategori DESC;`;
+    conn.query(sql, (err, results) => {
+        if(err) throw err;
+
+        res.send(results);
+        console.log(results);
     })
 })
 
