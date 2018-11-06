1c81bf0202d55bc03bb89898680c6271e3fdb4a6
Kevin <kevin_a_ferrari@hotmail.com>
2018-11-06 23:38:20 +0700

    commit 6 Nov 2018

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('/keeplogin', (req,res) => {
 });
 
 app.get('/listmenu', (req,res) => {
-    var sql1 = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+    var sql1 = `select idmenu, menu, description, harga, k.nama as kategori, images
                 from daftarmenu d join kategori k
                 on d.idkategori = k.id;`;
     var sql2 = `select * from kategori;`;
@@ -85,7 +85,7 @@ app.get('/listmenu', (req,res) => {
 
 app.get('/filterCatgr', (req,res) => {
     const { namakategori } = req.query;
-    var sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+    var sql = `select idmenu, menu, description, harga, k.nama as kategori, images
                 from daftarmenu d join kategori k
                 on d.idkategori = k.id
                 where k.nama Like '%${namakategori}%';`;
@@ -93,14 +93,14 @@ app.get('/filterCatgr', (req,res) => {
         if(err) throw err;
 
         res.send(results);
-        console.log(results);
+        // console.log(results);
     })
 });
 
 app.get('/sorthargaAsc', (req,res) => {
     const { namakategori } = req.query;
     if(namakategori !== ""){
-        var sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+        var sql = `select idmenu, menu, description, harga, k.nama as kategori, images
                     from daftarmenu d join kategori k
                     on d.idkategori = k.id
                     WHERE k.nama LIKE '%${namakategori}%'
@@ -113,7 +113,7 @@ app.get('/sorthargaAsc', (req,res) => {
         })
     }
     else{
-        sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+        sql = `select idmenu, menu, description, harga, k.nama as kategori, images
                 from daftarmenu d join kategori k
                 on d.idkategori = k.id
                 order by harga;`;
@@ -129,7 +129,7 @@ app.get('/sorthargaAsc', (req,res) => {
 app.get('/sorthargaDesc', (req,res) => {
     const { namakategori } = req.query;
     if(namakategori !== ""){
-        var sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+        var sql = `select idmenu, menu, description, harga, k.nama as kategori, images
                     from daftarmenu d join kategori k
                     on d.idkategori = k.id
                     WHERE k.nama LIKE '%${namakategori}%'
@@ -142,7 +142,7 @@ app.get('/sorthargaDesc', (req,res) => {
         })
     }
     else{
-        sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+        sql = `select idmenu, menu, description, harga, k.nama as kategori, images
                     from daftarmenu d
                     join kategori k
                     on d.idkategori = k.id
@@ -159,7 +159,7 @@ app.get('/sorthargaDesc', (req,res) => {
 app.get('/sortmenuAsc', (req,res) => {
     const { namakategori } = req.query;
     if(namakategori !== ""){
-        var sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+        var sql = `select idmenu, menu, description, harga, k.nama as kategori, images
                     from daftarmenu d join kategori k
                     on d.idkategori = k.id
                     WHERE k.nama LIKE '%${namakategori}%'
@@ -173,7 +173,7 @@ app.get('/sortmenuAsc', (req,res) => {
         })
     }
     else{
-        sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+        sql = `select idmenu, menu, description, harga, k.nama as kategori, images
                 from daftarmenu d
                 join kategori k
                 on d.idkategori = k.id
@@ -190,7 +190,7 @@ app.get('/sortmenuAsc', (req,res) => {
 app.get('/sortmenuDesc', (req,res) => {
     const { namakategori } = req.query;
     if(namakategori !== ""){
-        var sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+        var sql = `select idmenu, menu, description, harga, k.nama as kategori, images
                     from daftarmenu d join kategori k
                     on d.idkategori = k.id
                     WHERE k.nama LIKE '%${namakategori}%'
@@ -203,7 +203,7 @@ app.get('/sortmenuDesc', (req,res) => {
         })
     }
     else{
-        sql = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+        sql = `select idmenu, menu, description, harga, k.nama as kategori, images
                 from daftarmenu d
                 join kategori k
                 on d.idkategori = k.id
@@ -219,19 +219,109 @@ app.get('/sortmenuDesc', (req,res) => {
 
 app.get('/searchmenu', (req,res) => {
     const { searchValue } = req.query;
-    var sql1 = `select iddaftarmenu, menu, description, harga, k.nama as kategori, images
+    var sql1 = `select idmenu, menu, description, harga, k.nama as kategori, images
                 from daftarmenu d join kategori k
                 on d.idkategori = k.id
                 where menu Like '%${searchValue}%';`;
     var sql2 = `select * from kategori;`;
     conn.query(sql1, (err1, results1) => {
         if(err1) throw err
-        console.log(results1.data);
+        // console.log(results1.data);
 
         conn.query(sql2, (err2, results2) => {
             res.send({ daftarmenu: results1, kategori: results2 });
-            console.log(results2.data);;
+            // console.log(results2.data);;
         })
+    })
+});
+
+app.get('/getMenuDetails', (req,res) => {
+    const { id } = req.query;
+    var sql = `select idmenu, menu, description, harga, k.nama as kategori, images
+                from daftarmenu d join kategori k
+                on d.idkategori = k.id
+                where idmenu = ${id};`;
+    conn.query(sql, (err, results) => {
+        if(err) throw err;
+        
+        res.send(results);
+        // console.log(results);
+    })
+});
+
+app.post('/cart', (req,res) => {
+    const { username, idmenu, menu, jumlah, harga, images } = req.body;
+    var data = {
+        username: username,
+        idmenu: idmenu,
+        menu: menu, 
+        jumlah: jumlah,
+        harga: harga,
+        images: images
+    };
+    var sql = `select * from cart where username = "${username}" AND idmenu = ${idmenu};`;
+    conn.query(sql, data, (err, results) => {
+        if(err) throw err;
+        if(results != "") {
+            var sql1 = `UPDATE cart SET jumlah = jumlah + ${jumlah} where username = "${username}" AND idmenu = ${idmenu};`;
+            conn.query(sql1, data, (err1, results1) => {
+                if(err1) throw err1;
+
+                res.send(results1);
+                // console.log(results1);
+            })
+        }
+        else {
+            var sql2 = 'INSERT INTO cart SET ?';
+            conn.query(sql2, data, (err2, results2) => {
+                if(err2) throw err2;
+        
+                res.send(results2);
+                // console.log(results2);
+            })
+        }
+    })
+});
+
+app.get('/cart', (req,res) => {
+    const { username } = req.query;
+
+    var sql = `select * from cart where username = "${username}";`;
+    conn.query(sql, (err, results) => {
+        if(err) throw err;
+
+        res.send(results);
+        // console.log(results);
+    })
+});
+
+app.delete('/cart/:id', (req, res) => {
+    const { username } = req.query;
+
+    var sql = 'DELETE FROM cart WHERE idcart = ' + req.params.id;
+    conn.query(sql, (err, results) => {
+        if(err) throw err;
+
+        var sql1 = `select * from cart where username = "${username}";`;
+        conn.query(sql1, (err1, results1) => {
+            if(err1) throw err1;
+
+            res.send(results1);
+        })
+    })
+});
+
+app.put('/cart/:id', (req, res) => {
+    const { jumlah } = req.body;
+    var data = {
+        jumlah: jumlah
+    };
+
+    var sql = `UPDATE cart SET ? WHERE idcart=${req.params.id};`;
+    conn.query(sql, data, (err, results) => {
+        if(err) res.send({ err, status: 'Error' });
+        
+        res.send(results);
     })
 });
 
